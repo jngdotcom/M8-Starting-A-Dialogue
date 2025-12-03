a1ae0839090a0baa9f17dbf554689c79ffdd55b4
jngdotcom <jaguo4@s.sfusd.edu>
2025-12-03 12:03:19 -0800

    ???

--- lessons/dialogue.gd
+++ lessons/dialogue.gd
@@ -1,113 +1,43 @@
 extends Control
 
-var expressions := {
-	"happy": preload ("res://assets/emotion_happy.png"),
-	"regular": preload ("res://assets/emotion_regular.png"),
-	"sad": preload ("res://assets/emotion_sad.png"),
-}
-
-var bodies := {
-	"sophia": preload ("res://assets/sophia.png"),
-	"pink": preload ("res://assets/pink.png")
-}
-
-## An array of dictionaries. Each dictionary has three properties:
-## - expression: a [code]Texture[/code] containing an expression
-## - text: a [code]String[/code] containing the text the character says
-## - character: a [code]Texture[/code] representing the character
-var dialogue_items: Array[Dictionary] = [
-	{
-		"expression": expressions["regular"],
-		"text": "I've been learning about [/wave]fish",
-		"character": bodies["sophia"],
-		"choice": {
-			"oh": 2,
-			"cool!": 1
-		}
-	},
-	{
-		"expression": expressions["regular"],
-		"text": "How has it been going?",
-		"character": bodies["pink"],
-		"choice": {
-			"bad": 2,
-			"good":1
-		}
-	},
-	{
-		"expression": expressions["sad"],
-		"text": "... Well... it is a little bit [shake]complicated[/shake]!",
-		"character": bodies["sophia"],
-		"choice": {
-			"have you thought aboutlear": 2,
-			"maybe you should quit": 1
-		}
-	},
-	{
-		"expression": expressions["sad"],
-		"text": "fish are evil",
-		"character": bodies["pink"],
-		"choice": {
-			"idk..": 2,
-			"thank you": 1
-		}
-	},
-]
-var current_item_index := 0
+@export var dialogue_items: Array[DialogueItem] = []
 
 ## UI element that shows the texts
 @onready var rich_text_label: RichTextLabel = %RichTextLabel
-## UI element that progresses to the next text
-
+## Audio player that plays voice sounds while text is being written
 @onready var audio_stream_player: AudioStreamPlayer = %AudioStreamPlayer
 ## The character
 @onready var body: TextureRect = %Body
 ## The Expression
 @onready var expression: TextureRect = %Expression
-
+## The container for buttons
 @onready var action_buttons_v_box_container: VBoxContainer = %ActionButtonsVBoxContainer
+
 
 func _ready() -> void:
 	show_text(0)
-	
-func create_button(choices_data: Dictionary) -> void:
-	for button in action_buttons_v_box_container.get_children():
-		button.queue_free()
-		
-	for choice_text in choices_data:
-		var button := Button.new()
-		action_buttons_v_box_container.add_child(button)
-		
-		button.text = choice_text
-		
-		var target_line_idx: int = choices_data[choice_text]
-		
-		if target_line_idx == -1:
-			button.pressed.connect(get_tree().quit)
-		else: 
-			button.pressed.connect(show_text.bind(target_line_idx))
-			
-		
-			
-## Draws the current text to the rich text element
+
+
+## Draws the selected text
+## [param current_item_index] Displays the currently selected index from the dialogue array
 func show_text(current_item_index: int) -> void:
 	# We retrieve the current item from the array
 	var current_item := dialogue_items[current_item_index]
+	# We set the initial visible ratio to the text to 0, so we can change it in the tween
+	rich_text_label.visible_ratio = 0.0
 	# from the item, we extract the properties.
 	# We set the text to the rich text control
 	# And we set the appropriate expression texture
-	rich_text_label.text = current_item["text"]
-	expression.texture = current_item["expression"]
-	body.texture = current_item["character"]
-	create_button(current_item["choice"])
-	# We set the initial visible ratio to the text to 0, so we can change it in the tween
-	rich_text_label.visible_ratio = 0.0
+	rich_text_label.text = current_item.text
+	expression.texture = current_item.expression
+	body.texture = current_item.character
+	create_buttons(current_item.choices)
 	# We create a tween that will draw the text
 	var tween := create_tween()
 	# A variable that holds the amount of time for the text to show, in seconds
 	# We could write this directly in the tween call, but this is clearer.
 	# We will also use this for deciding on the sound length
-	var text_appearing_duration: float = current_item["text"].length() / 30.0
+	var text_appearing_duration := (current_item["text"] as String).length() / 30.0
 	# We show the text slowly
 	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
 	# We randomize the audio playback's start time to make it sound different
@@ -120,19 +50,34 @@ func show_text(current_item_index: int) -> void:
 	audio_stream_player.play(sound_start_position)
 	# We make sure the sound stops when the text finishes displaying
 	tween.finished.connect(audio_stream_player.stop)
-
+	
 	# We animate the character sliding in.
 	slide_in()
-
-
+	# We disable all buttons until the tween completes
 	for button: Button in action_buttons_v_box_container.get_children():
 		button.disabled = true
-	
-	tween.finished.connect(func() -> void:
+	tween.finished.connect( func() -> void:
 		for button: Button in action_buttons_v_box_container.get_children():
 			button.disabled = false
-)
+	)
+	
 
+
+## Adds buttons to the buttons container
+## [param choices_data] An array of [DialogChoice]
+func create_buttons(choices_data: Array[DialogueChoice]) -> void:
+	for button in action_buttons_v_box_container.get_children():
+		button.queue_free()
+	for choice in choices_data:
+		var button := Button.new()
+		action_buttons_v_box_container.add_child(button)
+		button.text = choice.text
+		
+		if choice.is_quit == true:
+			button.pressed.connect(get_tree().quit)
+		else:
+			var target_line_idx := choice.target_line_idx
+			button.pressed.connect(show_text.bind(target_line_idx))
 
 ## Animates the character when they start talking
 func slide_in() -> void:
